34fcb4a977afa47ba59c67c907da7d19f2dda360
Abishek Puri <Abishek@42-2-215-096.static.netvigator.com>
2016-04-06 03:15:50 +0800

    Shifted to pgp

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var express = require('express');
+var pgp = require('pg-promise')();
+var bodyParser = require("body-parser");
 var app = express();
 
+var db = pgp(process.env.DATABASE_URL);
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res) {
+  res.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
